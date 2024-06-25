17b5780685e2c5eab9b9e7e21439b0b6a42850bb
Tom Wright <tom.suffern.wright@gmail.com>
2024-06-25 19:17:04 +1000

    feat: server instance

---
+++ assets/userdata/server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -xe
+
+# install dependencies
+
+apt update
+apt install unzip
+
+# install aws cli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+aws --version
+
+# download assets
+
+bucket=$1
+mkdir -p assets
+aws s3 cp --recursive s3://$bucket/server assets
+
+# set up hostname and hosts file
+
+hostnamectl hostname server
+sed -i 's/^127.0.1.1.*/127.0.1.1\tserver.kubernetes.local server/' /etc/hosts
+cat assets/hosts >> /etc/hosts
--- bin/app.ts
+++ bin/app.ts
@@ -2,10 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import * as path from "path";
+import * as fs from "fs";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { DockerImage } from 'aws-cdk-lib';
+import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, SubnetType, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'kubernetes-cdk-way');
@@ -25,3 +28,38 @@ new BucketDeployment(stack, 'DeployAssets', {
   ],
   destinationBucket: bucket,
 });
+
+// set up networking
+
+const machines = fs.readFileSync(`${__dirname}/../assets/machines.txt`, "utf8")
+const ipAddressFor = (machine: string) => {
+  const matchingLine = machines
+  .split("\n")
+  .find(m => m.trim().includes(machine))
+
+  if(!matchingLine)
+    throw new Error("Machine not listed in machines.txt")
+
+  return matchingLine.split(" ")[0]
+}
+
+const vpc = new Vpc(stack, 'Vpc', {
+  cidr: "10.0.0.0/27",
+  maxAzs: 1,
+})
+
+// configure server instance
+
+const serverUserData = UserData.custom(
+  fs.readFileSync(`${__dirname}/../assets/userdata/server.sh`, "utf8"),
+)
+
+const serverInstance = new Instance(stack, "ServerInstance", {
+  vpc,
+  machineImage: MachineImage.fromSsmParameter("/aws/service/canonical/ubuntu/server/22.04/stable/current/arm64/hvm/ebs-gp2/ami-id"),
+  instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
+  userData: serverUserData,
+  privateIpAddress: ipAddressFor("server"),
+})
+serverInstance.role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"))
+bucket.grantRead(serverInstance)
