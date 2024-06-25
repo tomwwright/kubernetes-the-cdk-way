1147b51d9df6941f05c029bfafa1a458653919cd
Tom Wright <tom.suffern.wright@gmail.com>
2024-06-25 20:01:24 +1000

    feat: control plane bootstrap

--- assets/configurations.sh
+++ assets/configurations.sh
@@ -2,4 +2,10 @@
 
 set -xe
 
-cp kubernetes-the-hard-way/units/etcd.service out/server
+cp kubernetes-the-hard-way/units/etcd.service \
+  kubernetes-the-hard-way/units/kube-apiserver.service \
+  kubernetes-the-hard-way/units/kube-controller-manager.service \
+  kubernetes-the-hard-way/units/kube-scheduler.service \
+  kubernetes-the-hard-way/configs/kube-scheduler.yaml \
+  kubernetes-the-hard-way/configs/kube-apiserver-to-kubelet.yaml \
+  out/server
--- assets/userdata/server.sh
+++ assets/userdata/server.sh
@@ -44,4 +44,37 @@ systemctl start etcd
 
 etcdctl member list
 
+# install and bootstrap kubernetes control plane
 
+wget https://storage.googleapis.com/kubernetes-release/release/v1.28.3/bin/linux/arm64/kubectl \
+ https://storage.googleapis.com/kubernetes-release/release/v1.28.3/bin/linux/arm64/kube-apiserver \
+ https://storage.googleapis.com/kubernetes-release/release/v1.28.3/bin/linux/arm64/kube-controller-manager \
+ https://storage.googleapis.com/kubernetes-release/release/v1.28.3/bin/linux/arm64/kube-scheduler
+
+chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl  
+mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
+
+mkdir -p /etc/kubernetes/config /var/lib/kubernetes/
+
+mv ca.crt ca.key \
+  kube-api-server.key kube-api-server.crt \
+  service-accounts.key service-accounts.crt \
+  encryption-config.yaml \
+  /var/lib/kubernetes/
+
+mv kube-apiserver.service /etc/systemd/system/kube-apiserver.service
+mv kube-controller-manager.service /etc/systemd/system/
+mv kube-controller-manager.kubeconfig /var/lib/kubernetes/
+mv kube-scheduler.kubeconfig /var/lib/kubernetes/
+mv kube-scheduler.yaml /etc/kubernetes/config/
+mv kube-scheduler.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable kube-apiserver kube-controller-manager kube-scheduler
+systemctl start kube-apiserver kube-controller-manager kube-scheduler
+
+kubectl cluster-info --kubeconfig admin.kubeconfig
+
+# configure kubelet authorization
+
+kubectl apply -f kube-apiserver-to-kubelet.yaml --kubeconfig admin.kubeconfig
