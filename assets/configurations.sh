75d6a3e5f195be85d7cab49ce8b44e213cc827e3
Tom Wright <tom.suffern.wright@gmail.com>
2024-06-25 19:29:00 +1000

    feat: bootstrap etcd

---
+++ assets/configurations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -xe
+
+cp kubernetes-the-hard-way/units/etcd.service out/server
--- assets/userdata/server.sh
+++ assets/userdata/server.sh
@@ -25,3 +25,22 @@ aws s3 cp --recursive s3://$bucket/server assets
 hostnamectl hostname server
 sed -i 's/^127.0.1.1.*/127.0.1.1\tserver.kubernetes.local server/' /etc/hosts
 cat assets/hosts >> /etc/hosts
+
+# install and bootstrap etcd
+
+wget https://github.com/etcd-io/etcd/releases/download/v3.4.27/etcd-v3.4.27-linux-arm64.tar.gz
+tar -xvf etcd-v3.4.27-linux-arm64.tar.gz
+mv etcd-v3.4.27-linux-arm64/etcd* /usr/local/bin/
+
+mkdir -p /etc/etcd /var/lib/etcd
+chmod 700 /var/lib/etcd
+cp assets/ca.crt assets/kube-api-server.key assets/kube-api-server.crt /etc/etcd/
+
+mv assets/etcd.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable etcd
+systemctl start etcd
+
+etcdctl member list
+
+
