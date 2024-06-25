5027cce5f85f5f716836678a67b1354b7708bd44
Tom Wright <tom.suffern.wright@gmail.com>
2024-06-25 19:59:59 +1000

    refactor: work from directory

--- assets/userdata/server.sh
+++ assets/userdata/server.sh
@@ -19,12 +19,13 @@ aws --version
 bucket=$1
 mkdir -p assets
 aws s3 cp --recursive s3://$bucket/server assets
+cd assets
 
 # set up hostname and hosts file
 
 hostnamectl hostname server
 sed -i 's/^127.0.1.1.*/127.0.1.1\tserver.kubernetes.local server/' /etc/hosts
-cat assets/hosts >> /etc/hosts
+cat hosts >> /etc/hosts
 
 # install and bootstrap etcd
 
@@ -34,9 +35,9 @@ mv etcd-v3.4.27-linux-arm64/etcd* /usr/local/bin/
 
 mkdir -p /etc/etcd /var/lib/etcd
 chmod 700 /var/lib/etcd
-cp assets/ca.crt assets/kube-api-server.key assets/kube-api-server.crt /etc/etcd/
+cp ca.crt kube-api-server.key kube-api-server.crt /etc/etcd/
 
-mv assets/etcd.service /etc/systemd/system/
+mv etcd.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable etcd
 systemctl start etcd
